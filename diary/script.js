2bbaa7497809ec11ddf4cd361e533cd7864bd8b5
TreasureCanNot <tqmtimo@gmail.com>
2025-03-21 23:56:14 -0300

    idk how but I fixed it

--- diary/script.js
+++ diary/script.js
@@ -24,35 +24,7 @@ function controlPad(){
     }
 }
 
-function appearItAll(){
-    document.getElementById('showhide').style.display = 'block';
-    document.getElementById('changethething').innerHTML="Hide";
-    var element = document.getElementById('showhide');
-    element.id = "hideshow";
-    document.getElementById('changethething').onclick = disappearItAll;
-}
 
-function disappearItAll(){
-    document.getElementById('hideshow').style.display = 'none';
-    document.getElementById('changethething').innerHTML = "Show";
-    var element = document.getElementById('hideshow');
-    element.id = "showhide";
-    document.getElementById('changethething').onclick = appearItAll;
-}
-
-function toggleText() {
-    let textElement = document.getElementById('sh');
-    let button = document.getElementById('change');
-
-    if (textElement.style.display === "none") {
-        textElement.style.display = "block";
-        button.innerHTML = "Hide";
-    } 
-    else {
-        textElement.style.display = "none";
-        button.innerHTML = "Show";
-    }
-}
 
 
 
